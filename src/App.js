2917cb0b315de66d9f008549363af3efbacce234
Student 37 <student37@Codes-MacBook-Air-8.local>
2023-11-21 15:11:05 -0500

    pricingreact

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import PricingCard from "./components/PricingCard";
+import "./styles/PricingApp.css";
 
 function App() {
+  const [selectMonthly, setSelectMonthly] = useState(true);
+  console.log(selectMonthly);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="PricingApp">
+      <div className="app-container">
+        
+        <header>
+          <h1 className="header-topic">Our Pricing Plans</h1>
+          <div className="header-row">
+            <p>Annually</p>
+            <label className="price-switch">
+              <input
+                className="price-checkbox"
+                onChange={() => {
+                  setSelectMonthly((prev) => !prev);
+                }}
+                type="checkbox"
+              />
+              <div className="switch-slider"></div>
+            </label>
+            <p>Monthly</p>
+          </div>
+        </header>
+       
+        <div className="pricing-cards">
+          <PricingCard
+            title="Basic"
+            price={selectMonthly ? "19.99" : "199.99"}
+            storage="60 GB Storage"
+            users="5"
+            sendUp="5"
+          />
+          <PricingCard
+            title="Basic Plus"
+            price={selectMonthly ? "34.99" : "399.99"}
+            storage="70 GB Storage"
+            users="10"
+            sendUp="10"
+          />
+          <PricingCard
+            title="High End"
+            price={selectMonthly ? "79.99" : "899.99"}
+            storage="90 GB Storage"
+            users="20"
+          />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/PricingCard.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "../styles/PricingCard.css";
+const PricingCard = ({ title, price, storage, users, sendUp }) => {
+  return (
+    <div className="PricingCard">
+      <header>
+        <p className="card-title">{title}</p>
+        <h1 className="card-price">{price}</h1>
+      </header>
+      
+      <div className="card-features">
+        <div className="card-storage">{storage}</div>
+        <div className="card-users-allowed">{users} users in total</div>
+        <div className="card-send-up">Send up to {sendUp}</div>
+      </div>
+      <a href="https://youtu.be/dQw4w9WgXcQ?si=RSXwNvl92krjYPyp">
+      <button className="card-btn">READ MORE</button>
+      </a>
+      
+    </div>
+  );
+};
+
+export default PricingCard;
